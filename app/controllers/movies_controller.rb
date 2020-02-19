8bfabc58c7a6ccaed78e330f2d895d39b269cfa4
EC2 Default User <ec2-user@ip-172-31-39-218.us-east-2.compute.internal>
2020-02-19 23:38:56 +0000

    Added sort movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,11 +14,11 @@ class MoviesController < ApplicationController
   
   def index
     @all_ratings = ["G", "PG", "PG-13", "R"]
-    
+    @movies = Movie.all()
     if params[:sort] == "title" 
-  		@movies = @movies.order(:title).all()
-  	elsif params[:sort] == "release_date" 
-  		@movies = @movies.order(:release_date).all()
+  		@movies = Movie.order(:title).all()
+  	if params[:sort] == "release_date" 
+  		@movies = Movie.order(:release_date).all()
   	end
   	
   end
